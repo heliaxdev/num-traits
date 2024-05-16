3862aea4a607097a27ad7efed2d36ed9f00eda6b
Murisi Tarusenga <murisit@gmail.com>
2024-05-16 11:20:51 +0200

    Stop requiring that unchecked arithmetic operators be implemented.

--- src/ops/overflowing.rs
+++ src/ops/overflowing.rs
@@ -1,12 +1,36 @@
-use core::ops::{Add, Mul, Sub};
 use core::{i128, i16, i32, i64, i8, isize};
 use core::{u128, u16, u32, u64, u8, usize};
 
 macro_rules! overflowing_impl {
     ($trait_name:ident, $method:ident, $t:ty) => {
-        impl $trait_name for $t {
+        impl $trait_name<$t> for $t {
+            type Output = $t;
             #[inline]
-            fn $method(&self, v: &Self) -> (Self, bool) {
+            fn $method(self, v: $t) -> ($t, bool) {
+                <$t>::$method(self, v)
+            }
+        }
+
+        impl $trait_name<&$t> for $t {
+            type Output = $t;
+            #[inline]
+            fn $method(self, v: &$t) -> ($t, bool) {
+                <$t>::$method(self, *v)
+            }
+        }
+
+        impl $trait_name<$t> for &$t {
+            type Output = $t;
+            #[inline]
+            fn $method(self, v: $t) -> ($t, bool) {
+                <$t>::$method(*self, v)
+            }
+        }
+
+        impl $trait_name<&$t> for &$t {
+            type Output = $t;
+            #[inline]
+            fn $method(self, v: &$t) -> ($t, bool) {
                 <$t>::$method(*self, *v)
             }
         }
@@ -14,10 +38,11 @@ macro_rules! overflowing_impl {
 }
 
 /// Performs addition with a flag for overflow.
-pub trait OverflowingAdd: Sized + Add<Self, Output = Self> {
+pub trait OverflowingAdd<Rhs = Self>: Sized {
+    type Output;
     /// Returns a tuple of the sum along with a boolean indicating whether an arithmetic overflow would occur.
     /// If an overflow would have occurred then the wrapped value is returned.
-    fn overflowing_add(&self, v: &Self) -> (Self, bool);
+    fn overflowing_add(self, v: Rhs) -> (Self::Output, bool);
 }
 
 overflowing_impl!(OverflowingAdd, overflowing_add, u8);
@@ -35,10 +60,11 @@ overflowing_impl!(OverflowingAdd, overflowing_add, isize);
 overflowing_impl!(OverflowingAdd, overflowing_add, i128);
 
 /// Performs substraction with a flag for overflow.
-pub trait OverflowingSub: Sized + Sub<Self, Output = Self> {
+pub trait OverflowingSub<Rhs = Self>: Sized {
+    type Output;
     /// Returns a tuple of the difference along with a boolean indicating whether an arithmetic overflow would occur.
     /// If an overflow would have occurred then the wrapped value is returned.
-    fn overflowing_sub(&self, v: &Self) -> (Self, bool);
+    fn overflowing_sub(self, v: Rhs) -> (Self::Output, bool);
 }
 
 overflowing_impl!(OverflowingSub, overflowing_sub, u8);
@@ -56,10 +82,11 @@ overflowing_impl!(OverflowingSub, overflowing_sub, isize);
 overflowing_impl!(OverflowingSub, overflowing_sub, i128);
 
 /// Performs multiplication with a flag for overflow.
-pub trait OverflowingMul: Sized + Mul<Self, Output = Self> {
+pub trait OverflowingMul<Rhs = Self>: Sized {
+    type Output;
     /// Returns a tuple of the product along with a boolean indicating whether an arithmetic overflow would occur.
     /// If an overflow would have occurred then the wrapped value is returned.
-    fn overflowing_mul(&self, v: &Self) -> (Self, bool);
+    fn overflowing_mul(self, v: Rhs) -> (Self::Output, bool);
 }
 
 overflowing_impl!(OverflowingMul, overflowing_mul, u8);
@@ -78,14 +105,14 @@ overflowing_impl!(OverflowingMul, overflowing_mul, i128);
 
 #[test]
 fn test_overflowing_traits() {
-    fn overflowing_add<T: OverflowingAdd>(a: T, b: T) -> (T, bool) {
-        a.overflowing_add(&b)
+    fn overflowing_add<T: OverflowingAdd<Output = T>>(a: T, b: T) -> (T, bool) {
+        a.overflowing_add(b)
     }
-    fn overflowing_sub<T: OverflowingSub>(a: T, b: T) -> (T, bool) {
-        a.overflowing_sub(&b)
+    fn overflowing_sub<T: OverflowingSub<Output = T>>(a: T, b: T) -> (T, bool) {
+        a.overflowing_sub(b)
     }
-    fn overflowing_mul<T: OverflowingMul>(a: T, b: T) -> (T, bool) {
-        a.overflowing_mul(&b)
+    fn overflowing_mul<T: OverflowingMul<Output = T>>(a: T, b: T) -> (T, bool) {
+        a.overflowing_mul(b)
     }
     assert_eq!(overflowing_add(5i16, 2), (7, false));
     assert_eq!(overflowing_add(i16::MAX, 1), (i16::MIN, true));
